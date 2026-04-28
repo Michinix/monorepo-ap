c9248e0802d3454aa09958caf14f6cf817ec258e
Michinix <michel_marcotte@orange.fr>
2026-04-28 16:04:26 +0200

    REF: Suppression du service prisma-studio et ajustement de la configuration du service web

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,33 +1,30 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import { PrimeReactProvider } from "primereact/api";
-import { Toaster } from "react-hot-toast";
-import SessionWrapper from "./components/Session";
-import { toastConfig } from "./config/toast.config";
-import "./globals.css";
-
-const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { PrimeReactProvider } from 'primereact/api'
+import { Toaster } from 'react-hot-toast'
+import SessionWrapper from './components/Session'
+import { toastConfig } from './config/toast.config'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - Fripouilles",
-    default: "Fripouilles",
+    template: '%s - Fripouilles',
+    default: 'Fripouilles',
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="fr">
-      <body className={`${jakarta.className} bg-gray-100`}>
+      <body className="bg-gray-100">
         <Toaster {...toastConfig} />
         <PrimeReactProvider>
           <SessionWrapper>{children}</SessionWrapper>
         </PrimeReactProvider>
       </body>
     </html>
-  );
+  )
 }
